5e95f8038caf3283dbafa3c97835203d511d006a
mintonofall <mintonofall@gmail.com>
2022-11-27 11:44:23 +0900

    contry flag

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-
   const [players, setPlayers] = useState([]);
   const [menSingleList, setMenSingleList] = useState([]);
   const [womenSingleList, setWomenSingleList] = useState([]);
@@ -37,37 +36,33 @@ function App() {
       <div className="teamList">
         <div className="teamCard teamOne">
           <h5 className="teamName">방대표팀</h5>
-          <h6 className="teamPlayers"> MS : {menSingleList[0].name}</h6>
-          <h6 className="teamPlayers"> WS : {womenSingleList[0].name}</h6>
-          <h6 className="teamPlayers"> MD : {menDoubleList[0].name}</h6>
-          <h6 className="teamPlayers"> WD : {womenDoubleList[0].name}</h6>
-          <h6 className="teamPlayers"> XD : {mixDoubleList[0].name}</h6>
+          <h6 className="players"> MS : <img src="/contryFlagIcon/DK.png" /> 악셀센</h6>
         </div>
         <div className="teamCard teamTwo">
-          <h5 className="teamName">모팀장팀</h5>
+          {/* <h5 className="teamName">모팀장팀</h5>
           <h6 className="teamPlayers"> MS : {menSingleList[1].name}</h6>
           <h6 className="teamPlayers"> WS : {womenSingleList[1].name}</h6>
           <h6 className="teamPlayers"> MD : {menDoubleList[1].name}</h6>
           <h6 className="teamPlayers"> WD : {womenDoubleList[1].name}</h6>
-          <h6 className="teamPlayers"> XD : {mixDoubleList[1].name}</h6>
+          <h6 className="teamPlayers"> XD : {mixDoubleList[1].name}</h6> */}
 
         </div>
         <div className="teamCard teamThree">
-          <h5 className="teamName">지금을살자팀</h5>
+          {/* <h5 className="teamName">지금을살자팀</h5>
           <h6 className="teamPlayers"> MS : {menSingleList[2].name}</h6>
           <h6 className="teamPlayers"> WS : {womenSingleList[2].name}</h6>
           <h6 className="teamPlayers"> MD : {menDoubleList[2].name}</h6>
           <h6 className="teamPlayers"> WD : {womenDoubleList[2].name}</h6>
-          <h6 className="teamPlayers"> XD : {mixDoubleList[2].name}</h6>
+          <h6 className="teamPlayers"> XD : {mixDoubleList[2].name}</h6> */}
 
         </div>
         <div className="teamCard teamFour">
-          <h5 className="teamName">초초수팀</h5>
+          {/* <h5 className="teamName">초초수팀</h5>
           <h6 className="teamPlayers"> MS : {menSingleList[3].name}</h6>
           <h6 className="teamPlayers"> WS : {womenSingleList[3].name}</h6>
           <h6 className="teamPlayers"> MD : {menDoubleList[3].name}</h6>
           <h6 className="teamPlayers"> WD : {womenDoubleList[3].name}</h6>
-          <h6 className="teamPlayers"> XD : {mixDoubleList[3].name}</h6>
+          <h6 className="teamPlayers"> XD : {mixDoubleList[3].name}</h6> */}
 
         </div>
       </div>
